917e67286dceacec535dfa307231cbf3bbeb8f86
akkayy <techakayy@gmail.com>
2022-05-29 14:04:53 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<v-main>
+			<v-img
+				src="@/assets/images/cooking.png"
+				width="80px"
+				class="ml-6 mr-6"
+			></v-img>
+		</v-main>
+	</v-app>
+</template>
+
+<script>
+	import HelloWorld from './components/HelloWorld.vue'
+
+	export default {
+		name: 'App',
+
+		components: {
+			HelloWorld
+		},
+
+		data: () => ({
+			//
+		})
+	}
+</script>
